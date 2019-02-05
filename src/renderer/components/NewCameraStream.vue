2ea3240786d05687a1b909c131c8fb39ca1f04f1
John Panos <johncpanos@gmail.com>
2019-02-05 15:34:26 -0800

    Added bittrate

--- src/renderer/components/NewCameraStream.vue
+++ src/renderer/components/NewCameraStream.vue
@@ -20,38 +20,49 @@ export default {
     }
   },
   mounted() {
+    var opaqueId = "streamingtest-"+Janus.randomString(12);
     Janus.init({
       console: console,
       debug: "all",
-      server: this.server,
       callback: () => {
         this.log('extension initialized');
         this.janus = new Janus({
-          plugin: 'janus.plugin.streaming',
-          server: this.server,
-          onremotestream: (stream) => {
-            this.log('remote stream responded');
-            Janus.addVideoStream(this.$refs.videoElement);
-            var videoTracks = stream.getVideoTracks();
-            if (videoTracks === null || videoTracks === undefined || videoTracks.length === 0) {
-              this.log('no data found in stream');
-            }
-            if (videoTracks && videoTracks.length) {
-              this.bitrateTimer = setInterval(() => {
-                // Display updated bitrate, if supported
-                var bitrate = streaming.getBitrate();
-                this.$emit('bitrate', bitrate)
-              }, 1000);
-            }
-          },
-          success: (pluginHandle) => {
-            this.streaming = pluginHandle;
-            this.log("Plugin attached! (" + streaming.getPlugin() + ", id=" + streaming.getId() + ")");
-            var body = { "request": "watch", id: this.stream };
-            streaming.send({ "message": body });
-          },
+          destroyed: function() {
+						window.location.reload();
+					},
           error: () => {
-            this.log("error attaching plugin");
+            this.log("error connecting janus");
+          },
+          success: () => {
+            this.janus.attach({
+              opaqueId: opaqueId,
+              plugin: 'janus.plugin.streaming',
+              server: this.server,
+              onremotestream: (stream) => {
+                this.log('remote stream responded');
+                Janus.addVideoStream(this.$refs.videoElement);
+                var videoTracks = stream.getVideoTracks();
+                if (videoTracks === null || videoTracks === undefined || videoTracks.length === 0) {
+                  this.log('no data found in stream');
+                }
+                if (videoTracks && videoTracks.length) {
+                  this.bitrateTimer = setInterval(() => {
+                    // Display updated bitrate, if supported
+                    var bitrate = streaming.getBitrate();
+                    this.$emit('status', 'bitrate: ' + bitrate);
+                  }, 1000);
+                }
+              },
+              success: (pluginHandle) => {
+                this.streaming = pluginHandle;
+                this.log("Plugin attached! (" + streaming.getPlugin() + ", id=" + streaming.getId() + ")");
+                var body = { "request": "watch", id: this.stream };
+                streaming.send({ "message": body });
+              },
+              error: () => {
+                this.log("error attaching plugin");
+              }
+            });
           }
         });
       },
